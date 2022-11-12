60df404282e5057e63643b0de4779e2364f44398
drmwnn <asep.darmawan879@gmail.com>
2022-11-12 20:16:43 +0700

    disable

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -116,3 +116,17 @@ function valMasuk() {
     }
 }
 
+//disable 
+document.onkeydown = function(e) {
+    if (e.ctrlKey && 
+        (e.keyCode === 67 || 
+         e.keyCode === 86 || 
+         e.keyCode === 85 || 
+         e.keyCode === 117)) {
+        
+        return false;
+    } else {
+        return true;
+    }
+};
+
