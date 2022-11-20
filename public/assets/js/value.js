45a20248124ff37ffdbe2ead41d6bdc39b8a81ab
drmwnn <asep.darmawan879@gmail.com>
2022-11-20 17:12:18 +0700

    value

--- public/assets/js/value.js
+++ public/assets/js/value.js
@@ -2,14 +2,12 @@ $(document).ready(function() {
     $("input[type='radio']").click(function() {
         var harga = $("input[name='denom']:checked").val();
         var payment = $("input[name='payment']:checked").val();
-        if (payment) {
-            console.log(payment);
-        }
+        
         fetch("https://produk.asepdarmawan3.repl.co").then((data) => {
             return data.json();
         }).then((objectData) => {
             let tableData = "";
-            console.log(objectData[0].price);
+            //console.log(objectData[0].price);
             tableData = `<b>Rp${objectData[harga].price}</b>`;
             document.getElementById("price_retail").innerHTML = tableData;
             document.getElementById("price_alfamart").innerHTML = tableData;
@@ -28,5 +26,8 @@ $(document).ready(function() {
             document.getElementById("price_bni").innerHTML = tableData;
             document.getElementById("price_mandiri").innerHTML = tableData;
         });
+
+        console.log(payment);
+
     });
 });
